b104acfbe598e5bcc2ae44a41d422bd5fd330254
kim-0617 <kkk5993@naver.com>
2023-05-14 22:06:38 +0900

    Create lineChart component
    react-chartjs-2 를 이용한 코인 가격변화 추이 라인그래프

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,72 @@
+import { Col, Row, Typography } from "antd";
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
+type LineChartType = {
+  coinHistory: any;
+  currentPrice: string;
+  coinName: string;
+};
+
+const { Title } = Typography;
+
+const LineChart = ({ coinHistory, currentPrice, coinName }: LineChartType) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    const date = new Date(coinHistory?.data?.history[i].timestamp * 1000);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const formattedDate = `${year}-${month}-${day}`;
+    coinTimestamp.push(formattedDate);
+  }
+
+  const data = {
+    labels: coinTimestamp.reverse(),
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart{" "}
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
+};
+
+export default LineChart;
