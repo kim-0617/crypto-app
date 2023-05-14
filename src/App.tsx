291202f5fd0851c35cea027df666d08b78ad7be2
kim-0617 <kkk5993@naver.com>
2023-05-14 22:09:22 +0900

    Add Components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
 import React from "react";
+import {
+  Navbar,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Space, Typography } from "antd";
 
 function App() {
   return (
     <div className="app">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
 
-      <div className="main"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
 
-      <div className="footer"></div>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "#fff", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
