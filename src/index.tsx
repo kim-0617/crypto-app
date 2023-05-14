e225cc9ca163363aabc88cfe8db8254fabf63c38
kim-0617 <kkk5993@naver.com>
2023-05-14 22:08:58 +0900

    Create App.tsx
    router, store 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./App.css";
+import store from "./app/store";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
